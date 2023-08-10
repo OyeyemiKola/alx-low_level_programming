0221b591c9b37a73e52a5e3d19b05516c5e5fc91
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-10 23:58:30 +0100

    6-size.c | Fixed the documetation | v 1.1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/** 
- * Main - Main block / beginning of code
- * Return 0 | Code is working with no error 
- * 
+/**
+ * main - Main block / beginning of code
+ * return: 0;
+ * Code is working with no error
+ *
  * This program would display the size of each datatype
  */
 
 
-int main(void)
+int main(void) /* main program */
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -16,8 +17,5 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	return (0);
+	return (0); /* it return 0 | code runs succesfully */
 }
-	
-
-
