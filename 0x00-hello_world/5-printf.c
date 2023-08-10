8fd6555701f02e454a0abc55998cfb27e2ed2507
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-10 23:34:29 +0100

    5-printf.c | The first version | v1.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This code sends the string
+ * with proper grammar, but the outcome is a piece of art,
+ * to the Standard Output.
+ *
+ **/
+
+int main(void) /* the main function */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* return value 0 whcih mean script runs without error */
+}
