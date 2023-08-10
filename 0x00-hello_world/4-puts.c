746da7cd193d22380987070b87bf7557e1056b15
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-10 23:11:34 +0100

    4-puts.c | Uploaded the c program | v 1.0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* This code sends the string 
+ * "Programming is like building a multilingual puzzle"
+ * to the Standard Output.
+ *
+ */
+
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+	
+
+
