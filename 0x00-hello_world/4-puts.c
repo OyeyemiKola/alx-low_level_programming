90b9009d75ad4073f8027f1dc6968bcadeaaeaf2
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-10 23:30:22 +0100

    4-puts.c | obeyed betty | v 1.11

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-/* This code sends the string 
+/**
+ * This code sends the string
  * "Programming is like building a multilingual puzzle"
  * to the Standard Output.
  *
- */
+ **/
 
-int main(void) 
+int main(void) /* the main function */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0); /* return value 0 whcih mean script runs without error */
 }
-	
-
-
