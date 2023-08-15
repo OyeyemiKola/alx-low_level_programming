cf4cfa5fab65486361ccbd266daf3f4e13774aca
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-15 20:50:51 +0100

    1-alphabet.c | Modified the main.h and added the files | v1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h" 
+
+/**
+ * main - it will print alphabet
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,3 +14,25 @@ int showPutchar(void)
 }
 
 
+/**
+ * 1. I sometimes suffer from insomnia. 
+ * And when I can't fall asleep, I play what I call the alphabet game
+ */
+
+int print_alphabet(void)
+{
+	char alph;
+
+	alph = 'a';
+
+	while (alph <= 'z')
+	{	
+		putchar(alph);
+		alph++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
