dab2dab908b908961f860b43a0b27623d8a343df
Kolawole Oyeyemi <realkolaoyeyemi@gmail.com>
2023-08-23 01:45:45 +0100

    Revert "Added the files via git push"
    This reverts commit d3b4773f664ada17b345e5dfe79e211e00a68584.

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,21 +1,15 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
-	char s3[] = "Hello cat";
+    char *str;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	printf("%d\n", _strcmp(s3, s1));
-	return (0);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
