d3b4773f664ada17b345e5dfe79e211e00a68584
Kolawole Oyeyemi <138940995+OyeyemiKola@users.noreply.github.com>
2023-08-23 01:42:22 +0100

    Added the files via git push
    Also updated the previous code

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,21 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
+
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+	char s1[] = "Hello";
+	char s2[] = "World!";
+	char s3[] = "Hello cat";
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	printf("%d\n", _strcmp(s3, s1));
+	return (0);
 }
